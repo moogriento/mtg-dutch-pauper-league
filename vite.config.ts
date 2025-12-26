eac0a5e4605147d119882017430f942d7367582c
Fernando Tong <nandorov@gmail.com>
2025-12-26 22:55:30 +0100

    another 404 trick

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+const basePath = process.env.VITE_BASE_URL;
+
+console.log('building with path:', basePath);
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL,
+  base: basePath,
   plugins: [
     tailwindcss(),
     react({
